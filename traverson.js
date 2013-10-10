16f4bc7b3baa16ecba463cc2719e48e41a134b84
Bastian Krol <bastian.krol@web.de>
2013-10-11 00:14:06 +0200

    wrap result of walk(..) and other refactorings

--- traverson.js
+++ traverson.js
@@ -3,6 +3,8 @@
 var ApiClient = require('./lib/api_client')
 var mediaTypes = require('./lib/media_types')
 
+//require('minilog').enable();
+
 exports.json = {
   from: function(startUri) {
     return new ApiClient(mediaTypes.JSON, startUri)
