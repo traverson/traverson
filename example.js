35022f0963fe47f881977db17cfb936207b5b1b4
Bastian Krol <bastian.krol@web.de>
2013-11-02 00:51:03 +0100

    Make example page a bit less ugly

--- example.js
+++ example.js
@@ -1,21 +1,115 @@
 'use strict';
 
-var rootUri = '/'
-
-window.onload = function () {
-  document.getElementById('general').innerHTML =
-    'var rootUri = ' + rootUri + '<br/>' +
-    'var jsonApi = traverson.<i>json</i>.from(rootUri)<br/>' +
-    'var jsonHalApi = traverson.<i>jsonHal</i>.from(rootUri)<br/>'
-};
-
 require(['traverson', 'lib/log'], function(traverson, log) {
+
+  var rootUri = '/'
 
   var jsonApi = traverson.json.from(rootUri)
   var jsonHalApi = traverson.jsonHal.from(rootUri)
 
+  function executeAllRequests() {
+    ['#plain_vanilla_response',
+     '#jsonpath_response',
+     '#uri_template_response',
+     '#json_hal_response'].forEach(function(div) {
+      $(div).html('<img src="spinner.gif"/>')
+    })
+    executePlainVanillaRequest()
+    executeJsonPathRequest()
+    executeUriTemplateRequest()
+    executeHalRequest()
+  }
+
   // plain vanilla link following
-  document.getElementById('plain_vanilla_request').innerHTML =
+  function executePlainVanillaRequest() {
+    $('#plain_vanilla_response').html('<img src="spinner.gif"/>')
+    jsonApi.newRequest()
+      .withRequestOptions({
+        headers: { 'accept': 'application/json' }
+      })
+      .walk('second', 'doc')
+      .getResource(function(err, resource) {
+        console.log(err)
+        if (err) {
+          $('#plain_vanilla_response').html(JSON.stringify(err))
+          return
+        }
+        console.log(resource)
+        $('#plain_vanilla_response').html(JSON.stringify(resource))
+    })
+  }
+
+  // JSONPath
+  function executeJsonPathRequest() {
+    $('#jsonpath_response').html('<img src="spinner.gif"/>')
+    jsonApi.newRequest()
+      .withRequestOptions({
+        headers: { 'accept': 'application/json' }
+      })
+      .walk('$.jsonpath.nested.key')
+      .getResource(function(err, resource) {
+        console.log(err)
+        if (err) {
+          $('#jsonpath_response').html(JSON.stringify(err))
+          return
+        }
+        console.log(resource)
+        $('#jsonpath_response').html(JSON.stringify(resource))
+    })
+  }
+
+  // URI templates
+  function executeUriTemplateRequest() {
+    $('#uri_template_response').html('<img src="spinner.gif"/>')
+    jsonApi.newRequest()
+      .withRequestOptions({
+        headers: { 'accept': 'application/json' }
+      })
+      .walk('uri_template')
+      .withTemplateParameters({param: 'foobar', id: 13})
+      .getResource(function(err, resource) {
+        console.log(err)
+        if (err) {
+          $('#uri_template_response').html(JSON.stringify(err))
+          return
+        }
+        console.log(resource)
+        $('#uri_template_response').html(JSON.stringify(resource))
+    })
+  }
+
+  // HAL
+  function executeHalRequest() {
+    $('#json_hal_response').html('<img src="spinner.gif"/>')
+    jsonHalApi.newRequest()
+      .withRequestOptions({
+        headers: { 'accept': 'application/hal+json' }
+      })
+      .walk('first', 'second', 'inside_second')
+      .getResource(function(err, resource) {
+        if (err) {
+          $('#json_hal_response').html(JSON.stringify(err))
+          return
+        }
+        $('#json_hal_response').html(JSON.stringify(resource))
+    })
+  }
+
+  $(document).ready(function () {
+    $('#btn-all').on('click', executeAllRequests)
+    $('#btn-plain-vanilla').on('click', executePlainVanillaRequest)
+    $('#btn-jsonpath').on('click', executeJsonPathRequest)
+    $('#btn-uri-template').on('click', executeUriTemplateRequest)
+    $('#btn-hal').on('click', executeHalRequest)
+    $('#general').html(
+      'var rootUri = ' + rootUri + '<br/>' +
+      'var jsonApi = traverson.<i>json</i>.from(rootUri)<br/>' +
+      'var jsonHalApi = traverson.<i>jsonHal</i>.from(rootUri)<br/>'
+    )
+  })
+
+  // plain vanilla link following
+  $('#plain_vanilla_request').html(
     'jsonApi.newRequest()<br/>' +
     '.withRequestOptions({<br/>' +
     '&nbsp;&nbsp;headers: { \'accept\': \'application/json\' }<br/>' +
@@ -24,25 +118,10 @@ require(['traverson', 'lib/log'], function(traverson, log) {
     '.getResource(function(err, resource) {<br/>' +
     '&nbsp;&nbsp;// do something with the resource...<br/>' +
     '})<br/>'
-  jsonApi.newRequest()
-    .withRequestOptions({
-      headers: { 'accept': 'application/json' }
-    })
-    .walk('second', 'doc')
-    .getResource(function(err, resource) {
-      console.log(err)
-      if (err) {
-        document.getElementById('plain_vanilla_response').innerHTML =
-            JSON.stringify(err)
-        return
-      }
-      console.log(resource)
-      document.getElementById('plain_vanilla_response').innerHTML =
-            JSON.stringify(resource)
-  })
+  )
 
   // JSONPath
-  document.getElementById('jsonpath_request').innerHTML =
+  $('#jsonpath_request').html(
     'jsonApi.newRequest()<br/>' +
     '.withRequestOptions({<br/>' +
     '&nbsp;&nbsp;headers: { \'accept\': \'application/json\' }<br/>' +
@@ -51,25 +130,10 @@ require(['traverson', 'lib/log'], function(traverson, log) {
     '.getResource(function(err, resource) {<br/>' +
     '&nbsp;&nbsp;// do something with the resource...<br/>' +
     '})<br/>'
-  jsonApi.newRequest()
-    .withRequestOptions({
-      headers: { 'accept': 'application/json' }
-    })
-    .walk('$.jsonpath.nested.key')
-    .getResource(function(err, resource) {
-      console.log(err)
-      if (err) {
-        document.getElementById('jsonpath_response').innerHTML =
-            JSON.stringify(err)
-        return
-      }
-      console.log(resource)
-      document.getElementById('jsonpath_response').innerHTML =
-            JSON.stringify(resource)
-  })
+  )
 
   // URI templates
-  document.getElementById('uri_template_request').innerHTML =
+  $('#uri_template_request').html(
     'jsonApi.newRequest()<br/>' +
     '.withRequestOptions({<br/>' +
     '&nbsp;&nbsp;headers: { \'accept\': \'application/json\' }<br/>' +
@@ -78,26 +142,10 @@ require(['traverson', 'lib/log'], function(traverson, log) {
     '.getResource(function(err, resource) {<br/>' +
     '&nbsp;&nbsp;// do something with the resource...<br/>' +
     '})<br/>'
-  jsonApi.newRequest()
-    .withRequestOptions({
-      headers: { 'accept': 'application/json' }
-    })
-    .walk('uri_template')
-    .withTemplateParameters({param: 'foobar', id: 13})
-    .getResource(function(err, resource) {
-      console.log(err)
-      if (err) {
-        document.getElementById('uri_template_response').innerHTML =
-            JSON.stringify(err)
-        return
-      }
-      console.log(resource)
-      document.getElementById('uri_template_response').innerHTML =
-            JSON.stringify(resource)
-  })
+  )
 
   // HAL
-  document.getElementById('json_hal_request').innerHTML =
+  $('#json_hal_request').html(
     'jsonHalApi.newRequest()<br/>' +
     '.withRequestOptions({<br/>' +
     '&nbsp;&nbsp;headers: { \'accept\': \'application/hal+json\' }<br/>' +
@@ -106,18 +154,5 @@ require(['traverson', 'lib/log'], function(traverson, log) {
     '.getResource(function(err, resource) {<br/>' +
     '&nbsp;&nbsp;// do something with the resource...<br/>' +
     '})<br/>'
-  jsonHalApi.newRequest()
-    .withRequestOptions({
-      headers: { 'accept': 'application/hal+json' }
-    })
-    .walk('first', 'second', 'inside_second')
-    .getResource(function(err, resource) {
-      if (err) {
-        document.getElementById('json_hal_response').innerHTML =
-            JSON.stringify(err)
-        return
-      }
-      document.getElementById('json_hal_response').innerHTML =
-            JSON.stringify(resource)
-  })
+  )
 })
