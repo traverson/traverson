b07b4d6080f001ce8899611b78dc71389db1f063
Bastian Krol <bastian.krol@web.de>
2014-04-29 20:38:41 +0200

    watchify

---
+++ bin/watchify.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# This script assumes that watchify is installed globally. To do that execute
+# npm install -g watchify
+
+# Three watchify processes are started in the background. Use
+# pkill -f watchify or pkill -f "node.*watchify"
+# to stop them.
+
+bin_path=`dirname $0`
+pushd $bin_path/.. > /dev/null
+
+watchify \
+  --entry traverson.js \
+  --outfile browser/dist/traverson.js \
+  --standalone traverson \
+  --debug \
+  --verbose \
+  &
+
+watchify \
+  --entry traverson.js \
+  --outfile browser/dist/traverson.external.js \
+  --require ./traverson \
+  --debug \
+  --verbose \
+  &
+
+watchify \
+  --entry browser/test/suite.js \
+  --outfile browser/test/browserified_tests.js \
+  --external ./traverson.js \
+  --debug \
+  --verbose \
+  &
+
+popd > /dev/null
