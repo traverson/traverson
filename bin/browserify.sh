24276989309f2db24063e0554ad0545ace36bc51
Bastian Krol <bastian.krol@web.de>
2014-10-28 07:34:32 +0100

    use browserify executable from node_modules/.bin

--- bin/browserify.sh
+++ bin/browserify.sh
@@ -3,18 +3,10 @@
 # Script to browserify without Grunt - usually the Grunt build is used to
 # browserify and build everything.
 
-# This script assumes that browserify is installed globally. If that is not the
-# case, one could also use the command
-# node_modules/grunt-browserify/node_modules/browserify/bin/cmd.js or
-# node_modules/browserify/bin/cmd.js
-# instead of `browserify`
-#
-# browserify_cmd=node_modules/browserify/bin/cmd.js
-# browserify_cmd=node_modules/grunt-browserify/node_modules/browserify/bin/cmd.js
-browserify_cmd=browserify
-
 bin_path=`dirname $0`
 pushd $bin_path/.. > /dev/null
+
+browserify_cmd=node_modules/.bin/browserify
 
 # This browserify build be used by users of the module. It contains a
 # UMD (universal module definition) and can be used via an AMD module
